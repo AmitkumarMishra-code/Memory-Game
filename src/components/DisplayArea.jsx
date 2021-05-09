55a0d4246c56998c47d0959034ef30c9d4ec65e1
Amitkumar Mishra <mr.mash@gmail.com>
2021-05-09 13:59:28 +0530

    fixed mobile design. added confetti.

--- src/components/DisplayArea.jsx
+++ src/components/DisplayArea.jsx
@@ -1,5 +1,20 @@
 import { useEffect, useState } from "react"
+import Confetti from 'react-dom-confetti'
 import GameGrid from "./GameGrid"
+
+const config = {
+    angle: 90,
+    spread: 360,
+    startVelocity: 40,
+    elementCount: 70,
+    dragFriction: 0.12,
+    duration: 3000,
+    stagger: 3,
+    width: "20px",
+    height: "20px",
+    perspective: "300px",
+    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+  };
 
 export default function DisplayArea(props) {
     let [gameOver, setGameOver] = useState(false)
@@ -22,8 +37,9 @@ export default function DisplayArea(props) {
     }, [props.gameStart])
     return (
         <div className="display-area">
-            {gameOver && <h1>Congratulations! You Won in {props.finalScore.time} seconds! You took {props.finalScore.moves} moves!</h1>}
+            {gameOver && <h1 className='win-message'>Congratulations! You Won in {props.finalScore.time} seconds! You took {props.finalScore.moves} moves!</h1>}
             {!gameOver && <GameGrid updateMoves={updateMoves} updateScore={updateScore} startGame = {props.startGame} firstClick = {props.firstClick}/>}
+            <Confetti active = {gameOver} config = {config}/>
         </div>
     )
 }
